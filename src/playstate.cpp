cfc06da9c17b505d6c1c383088e0e338eeb95e90
techel <thetechel@gmail.com>
2017-07-22 02:16:32 +0200

    camera follows player

--- src/playstate.cpp
+++ src/playstate.cpp
@@ -3,12 +3,16 @@
 #include "soundmanager.hpp"
 #include "player.hpp"
 
+Ticket t;
+
 PlayState::PlayState(App &app)
 {
 	app.sound().playMusic("test");
 
 	auto &player = TheMap.addEntity<Player>(TheMap, app);
 	player.setPosition({ 0.5f, 0.5f });
+
+	t = TheMap.camera().follow(&player);
 }
 
 void PlayState::tick(float seconds)
