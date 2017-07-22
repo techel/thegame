4ef8409233101b2b903094a691f99bcaeddc2bd6
Samuel Traue <samuel.traue@gmail.com>
2017-07-22 16:22:57 +0200

    removed logs, cleand merge confilicts

--- src/map.hpp
+++ src/map.hpp
@@ -14,38 +14,33 @@
 
 class Map : nonMovable, nonCopyable
 {
-public:
-	Map(App &app);
+	public:
+		Map(App &app);
 
-	IEntity &addEntity(std::unique_ptr<IEntity> entity);
+		IEntity &addEntity(std::unique_ptr<IEntity> entity);
 
-	template<class T, class... Args>
-	T &addEntity(Args&&... args)
-	{
-		auto entity = std::make_unique<T>(std::forward<Args>(args)...);
-		return static_cast<T&>(addEntity(std::move(entity)));
-	}
+		template<class T, class... Args>
+			T &addEntity(Args&&... args)
+			{
+				auto entity = std::make_unique<T>(std::forward<Args>(args)...);
+				return static_cast<T&>(addEntity(std::move(entity)));
+			}
 
-	void removeEntity(IEntity &e); //remove is delegated to the 'tick' method
+		void removeEntity(IEntity &e); //remove is delegated to the 'tick' method
 
-	Camera &camera() { return TheCamera; }
-	Physics &physics() { return ThePhysics; }
+		Camera &camera() { return TheCamera; }
+		Physics &physics() { return ThePhysics; }
 
-	PlayerController &player1() { return Player1Controller; }
-	PlayerController &player2() { return Player2Controller; }
-	
-	void tick(float seconds);
-	void render(sf::RenderTarget &target) const;
+		PlayerController &player1() { return Player1Controller; }
+		PlayerController &player2() { return Player2Controller; }
 
-private:
-	TaskQueue Deferred;
-	Camera TheCamera;
-<<<<<<< HEAD
-	Physics ThePhysics;
-=======
+		void tick(float seconds);
+		void render(sf::RenderTarget &target) const;
 
-    Physics ThePhysics;
->>>>>>> 060de62c467c4101364ccc13cda52b542b6f2e54
-	std::vector<std::unique_ptr<IEntity>> Entities;
-	PlayerController Player1Controller, Player2Controller;
+	private:
+		TaskQueue Deferred;
+		Camera TheCamera;
+		Physics ThePhysics;
+		std::vector<std::unique_ptr<IEntity>> Entities;
+		PlayerController Player1Controller, Player2Controller;
 };
